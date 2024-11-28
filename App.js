5a451e6f1885e6ff1b094985cbce6a6f508dcc59
DiegoCoria1 <diegofelipe.coria@alumnos.ulagos.cl>
2024-11-28 17:45:34 -0300

    Ejercicio 1 App (View + Text)

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar'; 
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+      <View style={{padding:3}}></View>
+      <LinearGradient
+        colors={['rgba(32,55,229,0.8)', 'transparent']}
+        style={styles.background}
+      />
+      <Text style={styles.text}>Ejercicio 1 uwu</Text>
+      <Text style={styles.smalltext}>Perdón la demora profe</Text>
+      <View style={{padding:3}}></View>
+      <StatusBar style="auto"/>
     </View>
-  );
-}
+    );
+  }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'aqua',
   },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 700,
+  },
+    text: {
+      color:'white',
+      fontSize: 20,
+      fontWeight: '600'
+  },
+  smalltext: {
+    color:'white',
+    fontSize: 12,
+    fontWeight: '600'
+},
 });
